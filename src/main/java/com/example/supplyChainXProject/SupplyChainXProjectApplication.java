292e113a75a6b0d318e1b7dfe9117efe0a672262
latifa <taybilatifa46@gmail.com>
2025-10-28 16:14:48 +0100

    configuration du spring Boot dans mon projet

---
+++ src/main/java/com/example/supplyChainXProject/SupplyChainXProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.supplyChainXProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyChainXProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyChainXProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/supplyChainXProject/SupplyChainXProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supplyChainXProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyChainXProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
